d89cd59a51cad7d0bc076069595d035817fbcc3c
rosy <lc_rosy@qq.com>
2026-05-13 15:51:31 +0800

    就绪队列实现优先级暂存

--- src/schedule/ready_queue.rs
+++ src/schedule/ready_queue.rs
@@ -1,8 +1,6 @@
 //! 就绪队列，实现为一个事件源
-//!
-//! todo：预存优先级，且考虑优先级的同步问题
 
-use core::sync::atomic::{AtomicIsize, Ordering};
+use core::sync::atomic::{AtomicIsize, AtomicU64, Ordering};
 
 use heapless::{mpmc::Queue, BinaryHeap, Deque};
 use spin::mutex::Mutex;
@@ -17,14 +15,26 @@ const PRIORITY_LEVELS: usize = (LOWEST_PRIORITY - HIGHEST_PRIORITY) as usize + 1
 pub(crate) struct ReadyQueue {
     /// index = priority - HIGHEST_PRIORITY
     queues: [Mutex<Deque<&'static TaskVirtImpl, READY_QUEUE_SIZE>>; PRIORITY_LEVELS],
-    // hightest_prio: AtomicIsize,
+    /// 最高优先级（HIGHEST_PRIORITY）安排在最高位（2^63），依次顺延
+    ///
+    /// 2^(63-PRIORITY_LEVELS)位（有效位之后的第一位）恒为1，之后的每位恒为0。
+    ///
+    /// 有效值：[2^(63-PRIORITY_LEVELS), 2^64-2^(63-PRIORITY_LEVELS)]
+    prio_bitmap: AtomicU64,
 }
 
 impl ReadyQueue {
     pub(crate) const fn new() -> Self {
+        assert!(PRIORITY_LEVELS <= 64);
+        let bitmap_init: u64 = if PRIORITY_LEVELS <= 63 {
+            1 << (63 - PRIORITY_LEVELS)
+        } else {
+            0
+        };
         Self {
             queues: [const { Mutex::new(Deque::new()) }; PRIORITY_LEVELS],
             // hightest_prio: AtomicIsize::new(LOWEST_PRIORITY + 1),
+            prio_bitmap: AtomicU64::new(bitmap_init),
         }
     }
 
@@ -37,37 +47,17 @@ impl ReadyQueue {
         self.queues[(prio - HIGHEST_PRIORITY) as usize]
             .lock()
             .push_back(task)?;
-
-        // loop {
-        //     let current_prio = self.hightest_prio.load(Ordering::Acquire);
-        //     if prio < current_prio {
-        //         if self
-        //             .hightest_prio
-        //             .compare_exchange(current_prio, prio, Ordering::AcqRel, Ordering::Acquire)
-        //             .is_ok()
-        //         {
-        //             break;
-        //         }
-        //     } else {
-        //         break;
-        //     }
-        // }
+        self.prio_bitmap
+            .fetch_or(1 << (63 - prio + HIGHEST_PRIORITY), Ordering::AcqRel);
         Ok(())
     }
 }
 
 impl EventSorce for ReadyQueue {
     fn hightest_priority(&self, _cpu_id: usize) -> isize {
-        // self.hightest_prio.load(Ordering::Acquire)
-        let mut prio = HIGHEST_PRIORITY;
-        while prio <= LOWEST_PRIORITY {
-            let queue = &self.queues[(prio - HIGHEST_PRIORITY) as usize];
-            if !queue.lock().is_empty() {
-                break;
-            }
-            prio += 1;
-        }
-        prio
+        let bitmap = self.prio_bitmap.load(Ordering::Acquire);
+        let highest_one = bitmap.highest_one().map_or(-1, |i| i as isize);
+        63 + HIGHEST_PRIORITY - highest_one
     }
 
     fn take_task(&self, _cpu_id: usize) -> (*const (), isize) {
@@ -77,16 +67,23 @@ impl EventSorce for ReadyQueue {
         while prio <= LOWEST_PRIORITY {
             let queue = &self.queues[(prio - HIGHEST_PRIORITY) as usize];
             if let Some(task) = queue.lock().pop_front() {
-                // // 更新优先级
-                let mut next_prio = prio;
-                while next_prio <= LOWEST_PRIORITY {
-                    let next_queue = &self.queues[(next_prio - HIGHEST_PRIORITY) as usize];
-                    if !next_queue.lock().is_empty() {
-                        break;
+                // 更新优先级
+                let next_prio = if queue.lock().is_empty() {
+                    let new_bitmap = self
+                        .prio_bitmap
+                        .fetch_and(!(1 << (63 - prio + HIGHEST_PRIORITY)), Ordering::AcqRel);
+                    // 再次检查队列为空，避免期间队列中插入了任务，但在位图上的置位被上文的fetch_and覆盖
+                    if queue.lock().is_empty() {
+                        let highest_one = new_bitmap.highest_one().map_or(-1, |i| i as isize);
+                        63 + HIGHEST_PRIORITY - highest_one
+                    } else {
+                        self.prio_bitmap
+                            .fetch_or(1 << (63 - prio + HIGHEST_PRIORITY), Ordering::AcqRel);
+                        prio
                     }
-                    next_prio += 1;
-                }
-                // self.hightest_prio.store(next_prio, Ordering::Release);
+                } else {
+                    prio
+                };
                 return (task.to_ptr(), next_prio);
             }
             prio += 1;
